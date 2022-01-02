8cd02051c93907fd24f480ac57afbd127df20887
kyouma_san <rohananand56@gmail.com>
2022-01-02 23:25:43 +0530

    setting signup route

--- server/app.js
+++ server/app.js
@@ -4,6 +4,10 @@ const app = express();
 const PORT = 5000;
 const {MONGOURI} = require('./keys');
 
+require('./models/user')
+app.use(express.json());
+app.use(require('./routes/auth'))
+
 
 mongoose.connect(MONGOURI);
 mongoose.connection.on('connected',()=>{
@@ -13,10 +17,6 @@ mongoose.connection.on('error',(err)=>{
     console.log("error faced while connecting to mongo: ", err);
 })
 //app.use(customMiddleware);        will use middelware for all the routes
-
-app.get('/', (req,res)=>{
-    res.send("hello world");
-})
 
 app.listen(PORT, ()=>{
     console.log("server is running on ", PORT);
