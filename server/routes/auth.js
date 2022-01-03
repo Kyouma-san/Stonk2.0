276c5bce0f982fde1b3fa47e8c9d2f6bb0c81c4d
kyouma_san <rohananand56@gmail.com>
2022-01-03 23:31:35 +0530

    signup api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,11 @@ router.get('/',(req,res)=>{
 })
 
 router.post('/signup',(req,res)=>{
-    console.log(req.body.name);
+    const {name,email,password} = req.body;
+    if(!email || !password || !name){
+        res.status(422).json({error:"please add all the fields"})
+    }
+    res.json({message:"successfully posted"});
 })
 
 module.exports = router;
