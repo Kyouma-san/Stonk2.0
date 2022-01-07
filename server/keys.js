e06cc1db0579fb1c18b307c5a4c799ef8ce917b6
kyouma_san <rohananand56@gmail.com>
2022-01-07 23:57:09 +0530

    minor changes

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MONGOURI: "mongodb+srv://rohan:Jr9Nh83mwrGTkKB@cluster0.jkufy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    MONGOURI: "mongodb+srv://rohan:Jr9Nh83mwrGTkKB@cluster0.jkufy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    JWT_SECRET:"gfasvyil5kJ45opiHI"
 }
