6f84cac307792b351bc6749acc589dd0d709cb2f
kyouma_san <rohananand56@gmail.com>
2022-01-08 22:11:44 +0530

    using bearer token

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,6 +6,10 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../keys')
 
+router.get('/protected', (req,res)=>{
+    res.send("hello user")
+})
+
 router.post('/signup', (req, res) => {
     const { name, email, password } = req.body;
     if (!email || !password || !name) {
