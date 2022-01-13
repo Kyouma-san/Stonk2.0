87bdeeffa471f5f96a171d4e53f62476e5319457
kyouma_san <rohananand56@gmail.com>
2022-01-14 03:44:59 +0530

    login page styling

--- client/src/components/screens/Login.js
+++ client/src/components/screens/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Login = () => {
     return (
         <div className="mycard">
-            <div className="card auth-card input-field">
+            <div className="card auth-card input-field #f1f8e9 light-green lighten-5">
                 <h2>Stonks</h2>
                 <input type="text" placeholder="email" />
                 <input type="text" placeholder="password" />
