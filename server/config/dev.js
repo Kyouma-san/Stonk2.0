8c8e0fd1c5ebdfa49a0fbaac43b7df1b758ca6ef
kyouma_san <rohananand56@gmail.com>
2022-01-22 00:59:23 +0530

    final prod version

--- server/config/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://rohan:Jr9Nh83mwrGTkKB@cluster0.jkufy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"gfasvyil5kJ45opiHI"
-}
